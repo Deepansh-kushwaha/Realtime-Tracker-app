4d505d73247ce9be746ed07299716a4da1523c5a
Deepansh kushwaha <deepanshkushwaha9@gmail.com>
2025-07-24 15:28:48 +0530

    Created a real time tracking application

---
+++ app.js
@@ -0,0 +1,30 @@
+const { render } = require('ejs');
+const express = require('express');
+const http = require('http');
+const socketio =  require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    socket.on('send-location', (data) => {
+        io.emit("recieved Location", {id: socket.id , ...data});
+    })
+    socket.on('disconnect', () => {
+        io.emit("user-disconnected", socket.id);
+    })
+    console.log('user connected');
+})
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+ 
+
+
+server.listen(3000);
